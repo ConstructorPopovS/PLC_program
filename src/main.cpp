9015c2e581999ea6b68ff4d165cf2025e2036799
Simeon <popovso1999@gmail.com>
2021-08-25 21:59:04 +0300

    Simple Control Modes on LCD

--- src/main.cpp
+++ src/main.cpp
@@ -4,58 +4,75 @@
 #include <Wire.h>              // библиотека для работы с I2C
 #include <LiquidCrystal_I2C.h> // библиотека для экранчика
 
-class LCDClass
-{
-public:
-  LCDClass();
-  LiquidCrystal_I2C lcd; // Задаем адрес и размерность дисплея.
-  void print(int row = 0, int column = 0);
-};
-LCDClass::LCDClass()
-{
-  LiquidCrystal_I2C lcd(0x27,20,4);
-  lcd.init();      // Инициализация lcd
-  lcd.backlight(); // Включаем подсветку
-  //Serial.println("Test");
-  delay(2000);
-}
-void LCDClass::print(int row, int column)
-{
-  lcd.setCursor(row, column);
-  lcd.print("lcd.print is working");
-}
-
 int x = 150;
 int y = 0;
 int fi = 0;
+modes targetMode;
+Flag flag;
 
 Robot robot(1, 2, 3, &Serial1, 4, 1000000L);
-LCDClass *lcd1 = nullptr;
-//LiquidCrystal_I2C lcd2(0x27, 20, 4);
+LiquidCrystal_I2C lcd(0x27, 20, 4);
 
 void setup(void)
 {
-  if (lcd1 == nullptr)
-  {
-    lcd1 = new LCDClass();
-  }
-  // lcd2.init();      // Инициализация lcd
-  // lcd2.backlight(); // Включаем подсветку
-  // lcd2.setCursor(0, 0);
-  // lcd2.print("Hallo");
+  lcd.init();      // Инициализация lcd
+  lcd.backlight(); // Включаем подсветку
   Serial.begin(9600);
-  // Serial.setTimeout(50); //установка времени для считывания из порта
-  //robot.setX(x);
-  // robot.setY(y);
-  delay(2000);
-  // Serial.println("getX = " + String(robot.getX()));
-  Serial.println("Priveeet");
+  Serial.setTimeout(50); //установка времени для считывания из порта
+  lcd.clear();
+  lcd.setCursor(3, 1);
+  lcd.print("Turn on the mode");
+  lcd.setCursor(5, 2);
+  lcd.print("use the key");
+  flag.currentMode = CLOSED;
+
+  pinMode(PIN_TOGGLE_FURNACE_MODES_HIGH, INPUT);
+  pinMode(PIN_TOGGLE_FURNACE_MODES_LOW, INPUT);
+  pinMode(PIN_TOGGLE_WITH_KEY_AUTOPILOT, INPUT);
+  pinMode(PIN_TOGGLE_WITH_KEY_MANUAL, INPUT);
 }
 
 /*--------------------------------------------------------------------*/
 void loop(void)
 {
-  lcd1->print();
-  Serial.println("Oy");
-  delay(1000);
+  if (digitalRead(PIN_TOGGLE_WITH_KEY_MANUAL))
+  {
+    targetMode = MANUAL;
+  }
+  else if (digitalRead(PIN_TOGGLE_WITH_KEY_AUTOPILOT))
+  {
+    targetMode = AUTOPILOT;
+  }
+  else
+  {
+    targetMode = CLOSED;
+  }
+  if (targetMode != flag.currentMode)
+  {
+    switch (targetMode)
+    {
+    case CLOSED:
+      lcd.clear();
+      lcd.setCursor(3, 1);
+      lcd.print("Turn on the mode");
+      lcd.setCursor(5, 2);
+      lcd.print("use the key");
+      flag.currentMode = CLOSED;
+      break;
+    case MANUAL:
+      lcd.clear();
+      lcd.setCursor(3, 1);
+      lcd.print("MANUAL MODE");
+      flag.currentMode = MANUAL;
+      break;
+    case AUTOPILOT:
+      lcd.clear();
+      lcd.setCursor(3, 1);
+      lcd.print("AUTOPILOT MODE");
+      flag.currentMode = AUTOPILOT;
+      break;
+    default:
+      break;
+    }
+  }
 }
--- src/main.h
+++ src/main.h
@@ -4,5 +4,39 @@
 #include <Arduino.h>
 #include <Dynamixel_Servo.h>
 
+// -----DEFINE PINS-----
+// Keypad
+#define PIN_C1 3
+#define PIN_C2 5
+#define PIN_C3 7
+#define PIN_C4 10
+#define PIN_R1 11
+#define PIN_R2 9
+#define PIN_R3 11
+#define PIN_R4 12
+#define PIN_R5 4
+#define PIN_R6 6
+#define PIN_R7 24
+#define PIN_R8 22
+#define PIN_R9 16
+#define PIN_R10 2
+// Toggle with key autopilot/manual
+#define PIN_TOGGLE_WITH_KEY_AUTOPILOT 29
+#define PIN_TOGGLE_WITH_KEY_MANUAL 27
+// Toggle furnace modes
+#define PIN_TOGGLE_FURNACE_MODES_HIGH 23
+#define PIN_TOGGLE_FURNACE_MODES_LOW 25
+// LCD connected to pins SDA, SCL, VCC, GND
 
+
+enum modes
+{
+  CLOSED,
+  MANUAL,
+  AUTOPILOT
+};
+struct Flag
+{
+  modes currentMode;
+};
 #endif
