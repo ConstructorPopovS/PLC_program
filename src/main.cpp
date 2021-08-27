60da00a68021ba4c027acb8f2d2e306b174ab38d
Simeon <popovso1999@gmail.com>
2021-08-27 12:16:33 +0300

    #include KeypadClass

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@ int fi = 0;
 modes targetMode;
 Flag flag;
 
-Robot robot(1, 2, 3, &Serial1, 4, 1000000L);
+Robot robot(1, 2, 3, &Serial1, 4, 1000000L);//TODO 1000000L change to long(1000000) and tested
 LiquidCrystal_I2C lcd(0x27, 20, 4);
 
 void setup(void)
@@ -21,10 +21,11 @@ void setup(void)
   Serial.setTimeout(50); //установка времени для считывания из порта
   lcd.clear();
   lcd.setCursor(3, 1);
-  lcd.print("Turn on the mode");
+  String text = "Turn on the mode";
+  lcd.print(text);
   lcd.setCursor(5, 2);
   lcd.print("use the key");
-  flag.currentMode = CLOSED;
+  flag.currentMode = BLOCKED;
 
   pinMode(PIN_TOGGLE_FURNACE_MODES_HIGH, INPUT);
   pinMode(PIN_TOGGLE_FURNACE_MODES_LOW, INPUT);
@@ -45,34 +46,48 @@ void loop(void)
   }
   else
   {
-    targetMode = CLOSED;
+    targetMode = BLOCKED;
   }
   if (targetMode != flag.currentMode)
   {
     switch (targetMode)
     {
-    case CLOSED:
+    case BLOCKED:
       lcd.clear();
-      lcd.setCursor(3, 1);
-      lcd.print("Turn on the mode");
-      lcd.setCursor(5, 2);
-      lcd.print("use the key");
-      flag.currentMode = CLOSED;
+      lcdSetCursorAndPrint(3, 1, "Turn on the mode");
+      lcdSetCursorAndPrint(5, 2, "use the key");
+      flag.currentMode = targetMode;
       break;
     case MANUAL:
       lcd.clear();
-      lcd.setCursor(3, 1);
-      lcd.print("MANUAL MODE");
-      flag.currentMode = MANUAL;
+      lcdSetCursorAndPrint(0, 0, "Man");
+      lcdSetCursorAndPrint(3, 0, String(x));
+      lcdSetCursorAndPrint(6, 0, ";");
+      lcdSetCursorAndPrint(7, 0, String(y));
+      lcdSetCursorAndPrint(10, 0, ";");
+      lcdSetCursorAndPrint(11, 0, String(fi));
+      lcdSetCursorAndPrint(14, 0, "SK");
+      lcdSetCursorAndPrint(16, 0, "*");
+      lcdSetCursorAndPrint(18, 0, "V");
+      lcd.setCursor(3, 0);
+      flag.currentMode = targetMode;
+      // manualControlMode();
       break;
     case AUTOPILOT:
       lcd.clear();
-      lcd.setCursor(3, 1);
-      lcd.print("AUTOPILOT MODE");
-      flag.currentMode = AUTOPILOT;
+      lcdSetCursorAndPrint(3, 1, "AUTOPILOT MODE");
+      flag.currentMode = targetMode;
       break;
     default:
       break;
     }
   }
+}
+void lcdSetCursorAndPrint(int column, int row, String text)
+{
+  lcd.setCursor(column, row);
+  lcd.print(text);
+}
+void manualControlMode()
+{
 }
--- src/main.h
+++ src/main.h
@@ -3,23 +3,10 @@
 
 #include <Arduino.h>
 #include <Dynamixel_Servo.h>
+#include "Keypad.h"
+#include "KeypadClass.h"
 
 // -----DEFINE PINS-----
-// Keypad
-#define PIN_C1 3
-#define PIN_C2 5
-#define PIN_C3 7
-#define PIN_C4 10
-#define PIN_R1 11
-#define PIN_R2 9
-#define PIN_R3 11
-#define PIN_R4 12
-#define PIN_R5 4
-#define PIN_R6 6
-#define PIN_R7 24
-#define PIN_R8 22
-#define PIN_R9 16
-#define PIN_R10 2
 // Toggle with key autopilot/manual
 #define PIN_TOGGLE_WITH_KEY_AUTOPILOT 29
 #define PIN_TOGGLE_WITH_KEY_MANUAL 27
@@ -31,7 +18,7 @@
 
 enum modes
 {
-  CLOSED,
+  BLOCKED,
   MANUAL,
   AUTOPILOT
 };
@@ -39,4 +26,7 @@ struct Flag
 {
   modes currentMode;
 };
+
+void lcdSetCursorAndPrint(int column, int row, String text);
+void manualControlMode();
 #endif
