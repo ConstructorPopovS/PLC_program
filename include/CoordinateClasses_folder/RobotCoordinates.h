128c03ca5df375e476cec11d10e56dd7961fc1b8
Simeon <popovso1999@gmail.com>
2021-09-02 02:47:45 +0300

    class RobotCoordinates has done

---
+++ include/CoordinateClasses_folder/RobotCoordinates.h
@@ -0,0 +1,48 @@
+#ifndef ROBOTCOORDINATES_H
+#define ROBOTCOORDINATES_H
+
+#include "../src/main.h"
+struct RobotCoord
+{
+    int x;
+    int y;
+    int fi;
+    int vX;
+    int vY;
+    int omegaFi;
+};
+struct BoolUpdateRobCoord
+{
+    bool x = false;
+    bool y = false;
+    bool fi = false;
+    bool vX = false;
+    bool vY = false;
+    bool omegaFi = false;
+};
+
+class RobotCoordinates
+{
+private:
+    RobotCoord _robotCoord;
+    RobotCoord _targetRobotCoord;
+    BoolUpdateRobCoord _boolRobCoordNeedUpdate;
+void hasCoordinateChanged(int *coordinate, int *targetCoordinate, bool *boolRobflag, bool *flag);
+protected:
+    virtual void renewCoordinates(RobotCoord targetRobotCoord);
+
+public:
+    RobotCoordinates(RobotCoord targetRobotCoord);
+    bool hasAnythingChanged(RobotCoord targetRobotCoord);
+    BoolUpdateRobCoord getBoolRobotCoordNeedUpdate();
+    void updateCoordinates();
+    void setRobotCoord(RobotCoord robotCoord);
+    void setTargetX(int targetX);
+    void setTargetY(int targetY);
+    void setTargetFi(int targetFi);
+    void setTargetVX(int targetVX);
+    void setTargetVY(int trrgetVY);
+    void setTargetOmegaFi(int targetOmegaFi);
+};
+
+#endif
---
+++ src/CoordinateClasses_folder/RobotCoordinates.cpp
@@ -0,0 +1,62 @@
+#include "../../include/CoordinateClasses_folder/RobotCoordinates.h"
+
+RobotCoordinates::RobotCoordinates(RobotCoord targetRobotCoord)
+{
+    _targetRobotCoord = targetRobotCoord;
+    _robotCoord = _targetRobotCoord;
+}
+void RobotCoordinates::hasCoordinateChanged(int *coordinate, int *targetCoordinate, bool *boolRobflag, bool *flag)
+{
+    if (coordinate != targetCoordinate)
+    {
+        *boolRobflag = true;
+        *flag = true;
+    }
+}
+bool RobotCoordinates::hasAnythingChanged(RobotCoord targetRobotCoord)
+{
+    bool flag = false;
+hasCoordinateChanged(&_robotCoord.x, &_targetRobotCoord.x, &_boolRobCoordNeedUpdate.x, &flag);
+hasCoordinateChanged(&_robotCoord.y, &_targetRobotCoord.y, &_boolRobCoordNeedUpdate.y, &flag);
+hasCoordinateChanged(&_robotCoord.fi, &_targetRobotCoord.fi, &_boolRobCoordNeedUpdate.fi, &flag);
+hasCoordinateChanged(&_robotCoord.vX, &_targetRobotCoord.vX, &_boolRobCoordNeedUpdate.vX, &flag);
+hasCoordinateChanged(&_robotCoord.vY, &_targetRobotCoord.vY, &_boolRobCoordNeedUpdate.vY, &flag);
+hasCoordinateChanged(&_robotCoord.omegaFi, &_targetRobotCoord.omegaFi, &_boolRobCoordNeedUpdate.omegaFi, &flag);
+    return (flag);
+}
+BoolUpdateRobCoord RobotCoordinates::getBoolRobotCoordNeedUpdate()
+{
+    return(_boolRobCoordNeedUpdate);
+}
+void RobotCoordinates::updateCoordinates()
+{
+    _robotCoord = _targetRobotCoord;
+}
+void RobotCoordinates::setRobotCoord(RobotCoord robotCoord)
+{
+    _targetRobotCoord = robotCoord;
+}
+void RobotCoordinates::setTargetX(int targetX)
+{
+_targetRobotCoord.x = targetX;
+}
+void RobotCoordinates::setTargetY(int targetY)
+{
+_targetRobotCoord.y = targetY;
+}
+void RobotCoordinates::setTargetFi(int targetFi)
+{
+_targetRobotCoord.fi = targetFi;
+}
+void RobotCoordinates::setTargetVX(int targetVX)
+{
+_targetRobotCoord.vX = targetVX;
+}
+void RobotCoordinates::setTargetVY(int targetVY)
+{
+_targetRobotCoord.vY = targetVY;
+}
+void RobotCoordinates::setTargetOmegaFi(int targetOmegaFi)
+{
+_targetRobotCoord.omegaFi = targetOmegaFi;
+}
