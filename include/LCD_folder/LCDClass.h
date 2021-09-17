021a146975180d5fa8ea8d747977b6a151ed9e2f
Simeon <popovso1999@gmail.com>
2021-09-17 12:03:18 +0300

    LCDClass is child class

--- include/LCD_folder/LCDClass.h
+++ include/LCD_folder/LCDClass.h
@@ -4,18 +4,11 @@
 #include <Wire.h>              // библиотека для работы с I2C
 #include <LiquidCrystal_I2C.h> // библиотека для экранчика
 
-class LCDClass
+class LCDClass : public LiquidCrystal_I2C
 {
 private:
-  LiquidCrystal_I2C _lcd;
-
 public:
   LCDClass();
-  void init();      // Инициализация lcd
-  void backlight(); // Включаем подсветку
-  void clear();
-  void setCursor(int column, int row);
-  void print(String text);
   void clearSetCursorAndPrint(int column, int row, String text);
   void setCursorAndPrint(int column, int row, String text);
 };
--- src/LCD_folder/LCDClass.cpp
+++ src/LCD_folder/LCDClass.cpp
@@ -1,37 +1,16 @@
 #include "../../include/LCD_folder/LCDClass.h"
 
-LCDClass::LCDClass() : _lcd(0x27, 20, 4)
+LCDClass::LCDClass() : LiquidCrystal_I2C(0x27, 20, 4)
 {
-}
-
-void LCDClass::init()
-{
-    _lcd.init();
-}
-void LCDClass::backlight()
-{
-    _lcd.backlight();
-}
-void LCDClass::clear()
-{
-    _lcd.clear();
-}
-void LCDClass::setCursor(int column, int row)
-{
-    _lcd.setCursor(column, row);
-}
-void LCDClass::print(String text)
-{
-    _lcd.print(text);
 }
 void LCDClass::clearSetCursorAndPrint(int column, int row, String text)
 {
-    _lcd.clear();
-    _lcd.setCursor(column, row);
-    _lcd.print(text);
+    clear();
+    setCursor(column, row);
+    print(text);
 }
 void LCDClass::setCursorAndPrint(int column, int row, String text)
 {
-    _lcd.setCursor(column, row);
-    _lcd.print(text);
+    setCursor(column, row);
+    print(text);
 }
